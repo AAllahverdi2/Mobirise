50eff771359aadf7d2818f9bfbca648e6ea51da6
Yasemen <yasaman.mammadli@gmail.com>
2024-08-09 16:15:59 +0400

    services section

---
+++ front/mobirise/src/components/Services/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './index.css'
+function Services() {
+  return (
+    <section className='services-section'>
+        <div className="container">
+            <div className="services-head-content">
+                <h3>Services</h3>
+                <p>
+                We offer a variety of floral services, from unique bouquets to event decoration.
+                </p>
+            </div>
+            <div className="mission">
+                <div className="mission-img">
+                    <img src="https://mobirise.com/extensions/floram4/floral-studio/assets/images/b4.jpg" alt="" />
+                </div>
+                <div className="mission-content">
+                    <h5>our mission</h5>
+                    <p>Lorem ipsum dolor sit amet, pri omnium verterem id, sit labore dicunt an, ea civibus.</p>
+                    <button>read more</button>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Services
